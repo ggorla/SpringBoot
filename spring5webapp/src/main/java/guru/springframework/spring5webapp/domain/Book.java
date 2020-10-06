448c167bc7fc94189c26e13ca04cfc77f3fcc619
Gautham Gorla <gauthamgorla@Gauthams-MacBook-Pro.local>
2020-10-06 17:51:20 -0400

    Commit publishing changes

--- spring5webapp/src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -3,8 +3,13 @@ package guru.springframework.spring5webapp.domain;
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
+
+/**
+ * Created by jt on 12/22/19.
+ */
 @Entity
 public class Book {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -12,20 +17,36 @@ public class Book {
     private String title;
     private String isbn;
 
+    @ManyToOne
+    private  Publisher publisher;
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
+    }
+
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
             inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
 
-
-    public Book(){
-
+    public Book() {
     }
 
     public Book(String title, String isbn) {
         this.title = title;
         this.isbn = isbn;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitle() {
@@ -44,21 +65,21 @@ public class Book {
         this.isbn = isbn;
     }
 
-    public Set<Author> getAuthor() {
+    public Set<Author> getAuthors() {
         return authors;
     }
 
-    public void setAuthor(Set<Author> author) {
-        this.authors = author;
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
     }
 
     @Override
     public String toString() {
         return "Book{" +
-                "title='" + title + '\'' +
+                "id=" + id +
+                ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
-                ", author=" + authors +
-                ", id=" + id +
+                ", authors=" + authors +
                 '}';
     }
 
