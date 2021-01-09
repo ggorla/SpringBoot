a73b31445220ba64ebd17fa0d3281f7aa6a26cc8
gauthamgorla <g1.gautham@gmail.com>
2021-01-09 10:33:35 -0500

    First commit for the JPA project

---
+++ SpringH2JPASample/src/main/java/com/example/SpringH2JPASample/SpringH2JpaSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringH2JPASample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringH2JpaSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringH2JpaSampleApplication.class, args);
+	}
+
+}
---
+++ SpringH2JPASample/src/test/java/com/example/SpringH2JPASample/SpringH2JpaSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringH2JPASample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringH2JpaSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
